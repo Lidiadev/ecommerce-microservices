235a514036a6594173a758072dacbd94b533b144
Lidia <necula_lidia@yahoo.com>
2020-10-25 23:49:52 +0100

    Add endpoints for the basked service

--- src/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Basket.API.Controllers
 {
@@ -6,5 +12,62 @@ namespace Basket.API.Controllers
     [ApiController]
     public class BasketController : ControllerBase
     {
+        private readonly IBasketRepository _repository;
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(IBasketRepository repository, ILogger<BasketController> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+
+            return Ok(basket ?? new BasketCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> UpdateBasket([FromBody]BasketCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            return Ok(await _repository.DeleteBasket(userName));
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // get total price of the basket
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+
+            if (basket == null)
+            {
+                _logger.LogError("Basket not exist with this user : {EventId}", basketCheckout.UserName);
+                return BadRequest();
+            }
+
+            // remove the basket 
+            var basketRemoved = await _repository.DeleteBasket(basketCheckout.UserName);
+            if (!basketRemoved)
+            {
+                _logger.LogError("Basket can not deleted");
+                return BadRequest();
+            }
+
+            return Accepted();
+        }
     }
 }
