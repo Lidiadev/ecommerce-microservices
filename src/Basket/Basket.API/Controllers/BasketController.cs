9da81736f6af062e60bff07f5a0e4ae70275887a
Lidia <necula_lidia@yahoo.com>
2020-10-25 23:08:06 +0100

    Create the Basket service

---
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+    }
+}
