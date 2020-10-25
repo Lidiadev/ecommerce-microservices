eeefa33e736fba0b4c248155baf49f3a11d91baf
Lidia <necula_lidia@yahoo.com>
2020-10-25 21:22:02 +0100

    Update settings name for catalog db

--- src/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Catalog.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CatalogController : ControllerBase
     {
