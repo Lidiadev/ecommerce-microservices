486be6077339c4201f1cfdbab360be5b83d7a633
Lidia <necula_lidia@yahoo.com>
2020-10-25 23:27:24 +0100

    Add basket context

---
+++ src/Basket/Basket.API/Data/BasketContext.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.Data.Interfaces;
+using StackExchange.Redis;
+
+namespace Basket.API.Data
+{
+    public class BasketContext : IBasketContext
+    {
+        public BasketContext(ConnectionMultiplexer redisConnection)
+        {
+            Redis = redisConnection.GetDatabase();
+        }
+
+        public IDatabase Redis { get; }
+    }
+}
