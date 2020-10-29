e83b70ad511fc27c134ff4ae58ec326e7d63e551
Lidia <necula_lidia@yahoo.com>
2020-10-29 23:26:03 +0100

    Add mapping between BasketCheckout and BasketCheckoutEvent

---
+++ src/Basket/Basket.API/Mappings/BasketMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+using EventBusRabbitMQ.Events;
+
+namespace Basket.API.Mappings
+{
+    public class BasketMapping : Profile
+    {
+        public BasketMapping()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
